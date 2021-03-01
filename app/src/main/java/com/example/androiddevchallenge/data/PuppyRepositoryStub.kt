7f5f0fadfb1a50de8d49ae21bbccba186c30cd24
Valentyn <trushchenko.v.wezom@gmail.com>
2021-03-01 15:16:16 +0200

    more gradle fixes

--- app/src/main/java/com/example/androiddevchallenge/data/PuppyRepositoryStub.kt
+++ app/src/main/java/com/example/androiddevchallenge/data/PuppyRepositoryStub.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.data
 
 import com.example.androiddevchallenge.R
@@ -8,9 +23,9 @@ class PuppyRepositoryStub : PuppyRepository {
     private val list by lazy {
         listOf(
             PuppyInfo(1, R.drawable.puppy1, "Apollo", desc, 5f, 30f, "Dark"),
-            PuppyInfo(2, R.drawable.puppy2, "Duke", desc,5f, 30f, "Light"),
-            PuppyInfo(3, R.drawable.puppy3, "Loki", desc,5f, 30f, "Light"),
-            PuppyInfo(4, R.drawable.puppy4, "Sam", desc,5f, 30f, "Light"),
+            PuppyInfo(2, R.drawable.puppy2, "Duke", desc, 5f, 30f, "Light"),
+            PuppyInfo(3, R.drawable.puppy3, "Loki", desc, 5f, 30f, "Light"),
+            PuppyInfo(4, R.drawable.puppy4, "Sam", desc, 5f, 30f, "Light"),
         )
     }
 
